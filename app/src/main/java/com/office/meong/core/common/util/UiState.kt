2609e7d76b3911defd372efbd3f49f934804156f
sonms <smsms5676@naver.com>
2026-07-08 17:59:58 +0900

    [FEAT#1] 공통 extension, util 세팅

---
+++ app/src/main/java/com/office/meong/core/common/util/UiState.kt
@@ -0,0 +1,19 @@
+package com.office.meong.core.common.util
+
+import androidx.compose.runtime.Stable
+
+@Stable
+sealed interface UiState<out T> {
+    data object Empty : UiState<Nothing>
+
+    data object Loading : UiState<Nothing>
+
+    data class Success<T>(
+        val data: T,
+    ) : UiState<T>
+
+    data class Failure(
+        val userMessage: String,
+        val throwable: Throwable? = null
+    ) : UiState<Nothing>
+}
