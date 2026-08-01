28103b4e21bec9f235c1ef46ed47a4e2d52708f4
Son Juwan <wndhks171@naver.com>
2026-08-01 15:53:38 +0900

    [feat/#12] login model

--- app/src/main/java/com/office/meong/presentation/auth/navigation/SignupNavigation.kt
+++ app/src/main/java/com/office/meong/presentation/auth/navigation/SignupNavigation.kt
@@ -6,7 +6,7 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
 import com.office.meong.core.navigation.Route
-import com.office.meong.presentation.onboarding.SignUpRoute
+import com.office.meong.presentation.auth.SignUpRoute
 import kotlinx.serialization.Serializable
 
 @Serializable
