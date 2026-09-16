8e260aac5dd5131f16185b9826fef50089ceb9d8
sonms <smsms5676@naver.com>
2026-09-16 14:46:59 +0900

    🚀1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         applicationId = "com.office.meong"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
+        versionCode = 10000
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
